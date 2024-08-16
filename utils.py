729f16afe1896edfd73eaabcf8d3b393c2f11b7c
Ronaldo Drecksler <estudansator@gmail.com>
2024-08-16 11:53:12 -0300

    gridseach mlp

---
+++ utils.py
@@ -0,0 +1,25 @@
+import itertools
+
+def gerar_arquiteturas():
+    # Número de nodos por camada oculta
+    nodos = list(range(21, 43))
+    
+    # Lista para armazenar as arquiteturas
+    arquiteturas = []
+    
+    # Arquiteturas com 1 camada oculta
+    for n in nodos:
+        arquiteturas.append([n])
+    
+    # Arquiteturas com 2 camadas ocultas
+    for n1, n2 in itertools.product(nodos, repeat=2):
+        arquiteturas.append([n1, n2])
+    
+    # # Arquiteturas com 3 camadas ocultas
+    # for n1, n2, n3 in itertools.product(nodos, repeat=3):
+    #     arquiteturas.append([n1, n2, n3])
+    
+    return arquiteturas
+
+
+
