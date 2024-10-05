89213d646945d33ee9f125db63c7e43101c73c5a
Ronaldo Drecksler <estudansator@gmail.com>
2024-10-04 23:56:22 -0300

    troca das arq

--- regression/gridSearch.py
+++ regression/gridSearch.py
@@ -55,151 +55,151 @@ for interation in tqdm(range(20), desc='Processing', unit='test'):
 
 ##########################################################################################################
 
-    # menor = np.inf
-    # for j in ("distance", "uniform"):
-    #     for i in range(1, 51):
-    #         KNN = KNeighborsRegressor(n_neighbors=i, weights=j)
-    #         KNN.fit(x_treino, y_treino)
-    #         opiniao = KNN.predict(x_validacao)
-    #         rmse = np.sqrt(mean_squared_error(y_validacao, opiniao))       
-    #         print("K: ", i, " Métrica: ", j, " RMSE: ", rmse)
-    #         if (rmse < menor):
-    #             menor = rmse
-    #             Melhor_k = i
-    #             Melhor_metrica = j
+    menor = np.inf
+    for j in ("distance", "uniform"):
+        for i in range(1, 51):
+            KNN = KNeighborsRegressor(n_neighbors=i, weights=j)
+            KNN.fit(x_treino, y_treino)
+            opiniao = KNN.predict(x_validacao)
+            rmse = np.sqrt(mean_squared_error(y_validacao, opiniao))       
+            print("K: ", i, " Métrica: ", j, " RMSE: ", rmse)
+            if (rmse < menor):
+                menor = rmse
+                Melhor_k = i
+                Melhor_metrica = j
 
 
-    # KNN = KNeighborsRegressor(n_neighbors=Melhor_k,weights=Melhor_metrica)
-    # KNN.fit(x_treino,y_treino)
-    # opiniao_KNN  = KNN.predict(x_teste)
+    KNN = KNeighborsRegressor(n_neighbors=Melhor_k,weights=Melhor_metrica)
+    KNN.fit(x_treino,y_treino)
+    opiniao_KNN  = KNN.predict(x_teste)
     
-    # rmse_KNN = np.sqrt(mean_squared_error(y_teste, opiniao))       
+    rmse_KNN = np.sqrt(mean_squared_error(y_teste, opiniao))       
 
-    # with open(log_knn, 'a+') as log_file:
-    #     log_file.write(f'{interation},{Melhor_k},{Melhor_metrica}\n')
+    with open(log_knn, 'a+') as log_file:
+        log_file.write(f'{interation},{Melhor_k},{Melhor_metrica}\n')
 
 ###########################################################################################################
 
-    # best_mlp_rmse = np.inf
-    # best_i_mlp = 0
-    # best_j_mlp = 0
-    # best_k_mlp = 0
-    # best_func_mlp = 0
-    # best_y_pred = 0
+    best_mlp_rmse = np.inf
+    best_i_mlp = 0
+    best_j_mlp = 0
+    best_k_mlp = 0
+    best_func_mlp = 0
+    best_y_pred = 0
 
 
-    # arquiteturas = gerar_arquiteturas()
+    arquiteturas = gerar_arquiteturas()
 
-    # for arq in arquiteturas:
-    #     for learning_rate in ('constant', 'invscaling', 'adaptive'):
-    #         for epocas in (100, 200, 300, 400, 500):
-    #             for l in ('identity', 'logistic', 'tanh', 'relu'):
+    for arq in arquiteturas:
+        for learning_rate in ('constant', 'invscaling', 'adaptive'):
+            for epocas in (100, 200, 300, 400, 500):
+                for l in ('identity', 'logistic', 'tanh', 'relu'):
 
-    #                 MLP = MLPRegressor(
-    #                     hidden_layer_sizes=arq, tol=1e-5, learning_rate=learning_rate, max_iter=epocas, activation=l)
+                    MLP = MLPRegressor(
+                        hidden_layer_sizes=arq, tol=1e-5, learning_rate=learning_rate, max_iter=epocas, activation=l)
                     
-    #                 MLP.fit(x_treino, y_treino)
+                    MLP.fit(x_treino, y_treino)
 
-    #                 opiniao = MLP.predict(x_validacao)
-    #                 rmse = np.sqrt(mean_squared_error(y_validacao, opiniao))       
+                    opiniao = MLP.predict(x_validacao)
+                    rmse = np.sqrt(mean_squared_error(y_validacao, opiniao))       
 
-    #                 if (rmse < best_mlp_rmse):
-    #                     best_mlp_rmse = rmse
-    #                     best_i_mlp = arq
-    #                     best_learning_rate_mlp = learning_rate
-    #                     best_epocas_mlp = epocas
-    #                     best_func_mlp = l
+                    if (rmse < best_mlp_rmse):
+                        best_mlp_rmse = rmse
+                        best_i_mlp = arq
+                        best_learning_rate_mlp = learning_rate
+                        best_epocas_mlp = epocas
+                        best_func_mlp = l
                     
 
 
             
 
 
-    # MLP = MLPRegressor(hidden_layer_sizes=best_i_mlp, learning_rate=best_learning_rate_mlp, max_iter=best_epocas_mlp, activation=best_func_mlp)
-    # MLP.fit(x_treino,y_treino)
-    # opiniao_MLP = MLP.predict(x_teste)
+    MLP = MLPRegressor(hidden_layer_sizes=best_i_mlp, learning_rate=best_learning_rate_mlp, max_iter=best_epocas_mlp, activation=best_func_mlp)
+    MLP.fit(x_treino,y_treino)
+    opiniao_MLP = MLP.predict(x_teste)
 
-    # rmse_MLP = np.sqrt(mean_squared_error(y_teste, opiniao_MLP))
+    rmse_MLP = np.sqrt(mean_squared_error(y_teste, opiniao_MLP))
 
-    # with open(log_mlp, 'a+') as log_file:
-    #     log_file.write(f'{interation},{best_i_mlp},{best_learning_rate_mlp},{best_epocas_mlp},{best_func_mlp}\n')
+    with open(log_mlp, 'a+') as log_file:
+        log_file.write(f'{interation},{best_i_mlp},{best_learning_rate_mlp},{best_epocas_mlp},{best_func_mlp}\n')
 
 # ##########################################################################################################
     ## SVM
 
 
-    # menor = np.inf
+    menor = np.inf
 
-    # # Grid search nos parâmetros kernel e C
-    # for kernel in ("linear", "poly", "rbf", "sigmoid"):
-    #     for C in [0.1, 1, 10, 100]: #retirei o 200
-    #         svm = SVR(kernel=kernel, C=C)
-    #         svm.fit(x_treino, y_treino)
-    #         opiniao = svm.predict(x_validacao)
+    # Grid search nos parâmetros kernel e C
+    for kernel in ("linear", "poly", "rbf", "sigmoid"):
+        for C in [0.1, 1, 10, 100]: #retirei o 200
+            svm = SVR(kernel=kernel, C=C)
+            svm.fit(x_treino, y_treino)
+            opiniao = svm.predict(x_validacao)
 
-    #         rmse = np.sqrt(mean_squared_error(y_validacao, opiniao))       
-    #         print(f"Kernel: {kernel}, C: {C}, RMSE: {rmse:.2f}")
-    #         if (rmse < menor):
-    #             menor = rmse
-    #             best_kernel = kernel
-    #             best_C = C
+            rmse = np.sqrt(mean_squared_error(y_validacao, opiniao))       
+            print(f"Kernel: {kernel}, C: {C}, RMSE: {rmse:.2f}")
+            if (rmse < menor):
+                menor = rmse
+                best_kernel = kernel
+                best_C = C
 
-    # svm = SVR(kernel=best_kernel, C=best_C)
-    # svm.fit(x_treino, y_treino)
-    # opiniao_SVM = svm.predict(x_teste)
-    # rmse_SVM = np.sqrt(mean_squared_error(y_teste, opiniao_SVM))
-    # with open(log_svm, 'a+') as log_file:
-    #     log_file.write(f'{1},{best_kernel},{best_C}\n')
+    svm = SVR(kernel=best_kernel, C=best_C)
+    svm.fit(x_treino, y_treino)
+    opiniao_SVM = svm.predict(x_teste)
+    rmse_SVM = np.sqrt(mean_squared_error(y_teste, opiniao_SVM))
+    with open(log_svm, 'a+') as log_file:
+        log_file.write(f'{1},{best_kernel},{best_C}\n')
 
 
 ##############################################################################
 
-    # melhor_rf_rmse = np.inf
-    # best_n_estimators = 0
-    # best_criterion = ''
-    # best_max_depth = 0
-    # best_min_samples_split = 0
-    # best_min_samples_leaf = 0
+    melhor_rf_rmse = np.inf
+    best_n_estimators = 0
+    best_criterion = ''
+    best_max_depth = 0
+    best_min_samples_split = 0
+    best_min_samples_leaf = 0
 
-    # for n_estimators in [50, 100, 150, 200]:
-    #     print(time.time())
-    #     for criterion in ['squared_error', 'absolute_error', 'friedman_mse', 'poisson']:
-    #         for max_depth in [10, 20, 30]:
-    #             for min_samples_split in [2, 5, 10]:
-    #                 for min_samples_leaf in [1, 2, 4]:
-    #                     rf = RandomForestRegressor(n_estimators=n_estimators, 
-    #                                                 criterion=criterion,
-    #                                                 max_depth=max_depth,
-    #                                                 min_samples_split=min_samples_split,
-    #                                                 min_samples_leaf=min_samples_leaf,
-    #                                                 random_state=42)
+    for n_estimators in [50, 100, 150, 200]:
+        print(time.time())
+        for criterion in ['squared_error', 'absolute_error', 'friedman_mse', 'poisson']:
+            for max_depth in [10, 20, 30]:
+                for min_samples_split in [2, 5, 10]:
+                    for min_samples_leaf in [1, 2, 4]:
+                        rf = RandomForestRegressor(n_estimators=n_estimators, 
+                                                    criterion=criterion,
+                                                    max_depth=max_depth,
+                                                    min_samples_split=min_samples_split,
+                                                    min_samples_leaf=min_samples_leaf,
+                                                    random_state=42)
 
-    #                     rf.fit(x_treino, y_treino)
-    #                     opiniao = rf.predict(x_validacao)
+                        rf.fit(x_treino, y_treino)
+                        opiniao = rf.predict(x_validacao)
 
-    #                     rmse_rf = np.sqrt(mean_squared_error(y_validacao, opiniao))
+                        rmse_rf = np.sqrt(mean_squared_error(y_validacao, opiniao))
 
-    #                     if rmse_rf < melhor_rf_rmse:
-    #                         melhor_rf_rmse = rmse_rf
-    #                         best_n_estimators = n_estimators
-    #                         best_criterion = criterion
-    #                         best_max_depth = max_depth
-    #                         best_min_samples_split = min_samples_split
-    #                         best_min_samples_leaf = min_samples_leaf
+                        if rmse_rf < melhor_rf_rmse:
+                            melhor_rf_rmse = rmse_rf
+                            best_n_estimators = n_estimators
+                            best_criterion = criterion
+                            best_max_depth = max_depth
+                            best_min_samples_split = min_samples_split
+                            best_min_samples_leaf = min_samples_leaf
 
-    # rf_final = RandomForestRegressor(n_estimators=best_n_estimators, 
-    #                                 criterion=best_criterion,
-    #                                 max_depth=best_max_depth,
-    #                                 min_samples_split=best_min_samples_split,
-    #                                 min_samples_leaf=best_min_samples_leaf,
-    #                                 random_state=42)
+    rf_final = RandomForestRegressor(n_estimators=best_n_estimators, 
+                                    criterion=best_criterion,
+                                    max_depth=best_max_depth,
+                                    min_samples_split=best_min_samples_split,
+                                    min_samples_leaf=best_min_samples_leaf,
+                                    random_state=42)
 
-    # rf_final.fit(x_treino, y_treino)
-    # opiniao_rf = rf_final.predict(x_teste)
-    # rmse_rf_final = np.sqrt(mean_squared_error(y_teste, opiniao_rf))
+    rf_final.fit(x_treino, y_treino)
+    opiniao_rf = rf_final.predict(x_teste)
+    rmse_rf_final = np.sqrt(mean_squared_error(y_teste, opiniao_rf))
 
-    # with open(log_rf, 'a+') as log_file:
-    #     log_file.write(f'{interation},{best_n_estimators},{best_criterion},{best_max_depth},{best_min_samples_split},{best_min_samples_leaf}\n')
+    with open(log_rf, 'a+') as log_file:
+        log_file.write(f'{interation},{best_n_estimators},{best_criterion},{best_max_depth},{best_min_samples_split},{best_min_samples_leaf}\n')
 
 
 ###########################################################################################################
